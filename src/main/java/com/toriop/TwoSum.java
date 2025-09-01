6a24a154e6262331eb6afd71fd25be8f05f0e1c9
TORIOP23 <toriop.dev@gmail.com>
2025-09-01 22:06:31 +0700

    problem: two sum

---
+++ src/main/java/com/toriop/TwoSum.java
@@ -0,0 +1,23 @@
+package com.toriop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * URL: <a href="https://leetcode.com/problems/two-sum/description/">Leetcode URL</a>
+ */
+public class TwoSum {
+
+    public int[] solve(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{i, map.get(complement)};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
